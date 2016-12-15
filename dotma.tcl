64ba436255e511fc0c967772301b03063e667cec
Kooda <kooda@upyum.com>
2016-12-15 20:08:16 +0100

    Better plumbing rules for PDF and image files

--- dotma.tcl
+++ dotma.tcl
@@ -1,15 +1,24 @@
 set current_fixed_font "terminus"
 set current_variable_font "Liberation Sans"
-
 set image_viewer "sxiv"
 set pdf_viewer "mupdf-x11"
 
 DefinePlumbing {^(.+)(.png|.jpg|.jpeg|.gif)} {
-    global image_viewer
-    exec $image_viewer [GetArg 0]
+    set fname [CanonicalFilename [GetArg 0]]
+    if {[file exists $fname]} {
+       global image_viewer
+       exec $image_viewer $fname
+       return 1
+    }
+    return 0
 }
 
 DefinePlumbing {^(.+).pdf} {
-    global pdf_viewer
-    exec $pdf_viewer [GetArg 0]
+    set fname [CanonicalFilename [GetArg 0]]
+    if {[file exists $fname]} {
+        global pdf_viewer
+        exec $pdf_viewer $fname
+        return 1
+    }
+    return 0
 }
